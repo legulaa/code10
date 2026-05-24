150cfbae8f8117156fb8e8193656f6f758ec808d
legulaa <m.podolskiy17@gmail.com>
2026-05-25 00:22:03 +0300

    Add files via upload

---
+++ dyson/js/main.js
@@ -0,0 +1,22 @@
+(function () {
+
+    // Бургер
+
+    document.addEventListener('click', burgerInit)
+
+    function burgerInit(e) {
+
+        const burgerIcon = e.target.closest('.burger-icon')
+        const burgerNavLink = e.target.closest('.header__nav-link')
+
+        if (!burgerIcon && !burgerNavLink) return
+        if (document.documentElement.clientWidth > 950) return
+
+        if (!document.body.classList.contains('body--opened-menu')) {
+            document.body.classList.add('body--opened-menu')
+        } else {
+            document.body.classList.remove('body--opened-menu')
+        }
+
+    }
+    })()

1bce2db34013d3b9e6dbe1a3700d9af497048a2c
legulaa <m.podolskiy17@gmail.com>
2026-06-03 11:17:17 +0300

    Add files via upload

---
+++ js__1/main.js
@@ -0,0 +1,12 @@
+const firstName = "Ivan";
+const lastName = "Ivanov";
+const student = true;
+
+let age = 20;
+let currentYear = 2026;
+let birthYear = currentYear - age;
+console.log(birthYear); 
+
+console.log(`Меня зовут`,firstName ,lastName ,`мне`, age, `лет. Я ученик курса:`, student )
+
+// res = 123456789falsetrue

37a4dca324228da5d10679d88f2ab94876de8e55
legulaa <m.podolskiy17@gmail.com>
2026-07-21 15:08:58 +0300

    Add files via upload

---
+++ js__76/js/main.js
@@ -0,0 +1,69 @@
+"use strict";
+
+const todoKeys = {
+  id: "id",
+  text: "text",
+  is_completed: "is_completed",
+};
+
+const todos = [];
+
+const errTodoNotFound = (todoId) => `Todo with id ${todoId} not found`;
+
+const getNewTodoId = (todos) =>
+  todos.reduce((maxId, todo) => Math.max(maxId, todo[todoKeys.id]), 0) + 1;
+
+const createTodo = (todos, text) => {
+  const newTodo = {
+    [todoKeys.id]: getNewTodoId(todos),
+    [todoKeys.text]: text,
+    [todoKeys.is_completed]: false,
+  };
+  todos.push(newTodo);
+  return newTodo;
+};
+
+const completeTodoById = (todos, todoId) => {
+  const todo = todos.find((todo) => todo[todoKeys.id] === todoId);
+
+  if (!todo) {
+    console.error(errTodoNotFound(todoId));
+    return null;
+  }
+  todo[todoKeys.is_completed] = !todo[todoKeys.is_completed];
+  return todo;
+};
+
+const deleteTodoById = (todos, todoId) => {
+  const todoIndex = todos.findIndex((todo) => todo[todoKeys.id] === todoId);
+  if (todoIndex === -1) {
+    console.error(errTodoNotFound(todoId));
+    return todos;
+  }
+  todos.splice(todoIndex, 1);
+  return todos;
+};
+
+// При помощи метода querySelector получаем элементы .form, .input и .todos
+// Создаем функцию createTodoElement(text), которая будет создавать todo в виде разметки
+// Создаем функцию handleCreateTodo(todos, text), которая будет вызывать createTodo и createTodoElement
+
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
+const todosSel = document.querySelector(".todos");
+
+const createTodoElement = (text) => {
+  const newItem = document.createElement("li");
+  newItem.classList.add("todo");
+  newItem.textContent = text;
+  return newItem;
+};
+
+const handleCreateTodo = (todos, text) => {
+  if (!text.trim()) {
+    return;
+  }
+
+  const todoElement = createTodoElement(text);
+  todos.append(todoElement);
+};
